da678c9bdd760786b71572d8b3483a9ef93f42be
PlatinumBeing <67624423+PlatinumBeing@users.noreply.github.com>
2024-10-06 12:05:27 +0530

    Exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(s, num) {
+    let result = ""
 
+    if( num >= 0){
+        for(let i = 0; i < num; i++){   
+            result += s;
+        }
+        return result;
+    }
+    else{
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
