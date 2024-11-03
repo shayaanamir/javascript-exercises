34518835c32b38cfed336337bc4eaf3c69c07a10
PlatinumBeing <67624423+PlatinumBeing@users.noreply.github.com>
2024-11-03 19:16:01 +0530

    Exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(s) {
 
+    let result = ""
+
+    for(let i = s.length-1; i >= 0; i--){
+        result = result + s[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
