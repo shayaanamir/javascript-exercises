d395dbc5910e8a1fa4631ae4dc0743b08784cb9b
PlatinumBeing <67624423+PlatinumBeing@users.noreply.github.com>
2024-12-06 19:02:38 +0530

    exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n)
+    if(n < 0){
+        return 'OOPS'
+    }
+    if(n <= 1){
+        return n
+    }
+    return fibonacci(n-1) + fibonacci(n-2)
 };
 
 // Do not edit below this line
