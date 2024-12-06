5df99189ac335fffd2af662f152d026e940ebc7a
PlatinumBeing <67624423+PlatinumBeing@users.noreply.github.com>
2024-12-06 17:55:13 +0530

    exercise 7 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let result = (num - 32) * (5/9)
+
+  return parseFloat(result.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let result = (num*(9/5)) + 32
+
+  return parseFloat(result.toFixed(1))
 };
 
 // Do not edit below this line
