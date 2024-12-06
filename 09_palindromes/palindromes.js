ece4d187f70e75f487d226ad8d33973c653a30b4
PlatinumBeing <67624423+PlatinumBeing@users.noreply.github.com>
2024-12-06 18:53:13 +0530

    exercise 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    const alphanumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    const str = s.toLowerCase().split('').filter((char)=>alphanumeric.includes(char)).join('')
+
+    const reverseStr = str.split('').reverse().join('')
+
+    return str == reverseStr
 };
 
 // Do not edit below this line
