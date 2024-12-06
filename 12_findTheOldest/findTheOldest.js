d893c69cdb87e68ff554340f872ba260e67234af
PlatinumBeing <67624423+PlatinumBeing@users.noreply.github.com>
2024-12-06 19:26:32 +0530

    exercise 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const getAge = function(yearOfBirth, yearOfDeath){
+    if(!yearOfDeath){
+        yearOfDeath = new Date().getFullYear();
+    }
+    return yearOfDeath - yearOfBirth
+};
 
+const findTheOldest = function(people) {
+
+    let oldestPerson = people[0]
+    console.log(oldestPerson.name)
+    people.forEach(person => {
+        if (getAge(person.yearOfBirth, person.yearOfDeath) > getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath)){
+            oldestPerson = person
+        }
+    });
+
+    return oldestPerson
 };
 
 // Do not edit below this line
