22832739eb3aa1545ffc8b4b94186d87b9b318b6
PlatinumBeing <67624423+PlatinumBeing@users.noreply.github.com>
2024-12-06 17:34:42 +0530

    excercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function( a, b) {
+    let sum = 0
+    let min, max
+
+    if(Number.isInteger(a) && Number.isInteger(b) && a > 0 && b >0){
+        if(a > b){
+            min = b
+            max = a
+        }
+        else{
+            min = a
+            max = b
+        }
+        
+        for(let i = min; i <= max; i++){
+            sum += i
+        }
+    
+        return sum
+    }
+    else{
+        return 'ERROR'
+    }
+    
 
 };
 
